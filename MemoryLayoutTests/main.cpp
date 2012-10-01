90e2815e6128d3b69b1ce334d1124108962b1aca
silvansky <v.silvansky@gmail.com>
2012-10-01 13:27:13 +0400

    Initial Commit

---
+++ MemoryLayoutTests/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MemoryLayoutTests
+//
+//  Created by Valentine Gorshkov on 01.10.12.
+//  Copyright (c) 2012 silvansky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
